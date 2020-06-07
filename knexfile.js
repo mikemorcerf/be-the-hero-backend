d2d9be399367a6632f196c020315444095f2ac79
Michael Moura <mikemorcerf@gmail.com>
2020-06-07 15:42:18 -0400

    update connection file

--- knexfile.js
+++ knexfile.js
@@ -25,28 +25,17 @@ module.exports = {
   },
 
   staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     migrations: {
       tableName: 'knex_migrations'
-    }
+    },
+    ssl: true
   },
 
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
-    pool: {
-      min: 2,
-      max: 10
-    },
     migrations: {
       tableName: 'knex_migrations'
     },
